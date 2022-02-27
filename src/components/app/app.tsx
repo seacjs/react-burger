ac14cb77816d76d929b92d789daf04c347310b20
Шоленинов Евгений Андреевич <sholeninov.ea@gazprom-neft.ru>
2022-02-27 13:10:46 +0300

    sprint1/step1 refactor 2

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import AppHeader from './app-header/app-header';
-import styles from './app.module.css';
-import BurgerIngredients from './burger-ingredients/burger-ingredients';
-import BurgerConstructor from './burger-constructor/burger-constructor';
+import AppHeader from '../app-header/app-header';
+import BurgerIngredients from '../burger-ingredients/burger-ingredients';
+import BurgerConstructor from '../burger-constructor/burger-constructor';
 
 function App() {
   return (
--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -8,39 +8,41 @@ export default class BurgerConstructor  extends React.Component {
   render(): React.ReactNode {
     return (
       <React.Fragment>
-        <div className={styles.wrap + ' ' +styles.wrapTop}>
-            <ConstructorElement
-              type="top"
-              isLocked={true}
-              text={data[0].name + ' (верх)'}
-              price={data[0].price}
-              thumbnail={data[0].image}
-            />
-        </div>
-        <div className={styles.wrapMiddle+ " mt-4 mb-4"} >
-          {
-            data.filter(item => item.type !== 'bun').map(cnstructorElement => {
-              return (
-                <div key={cnstructorElement._id}>
-                  <DragIcon type="primary" />
-                  <ConstructorElement
-                    text={cnstructorElement.name}
-                    price={cnstructorElement.price}
-                    thumbnail={cnstructorElement.image}
-                  />
-                </div>
-              )
-            })
-          }
-        </div>
-        <div className={styles.wrap + ' ' +styles.wrapBottom}>
-            <ConstructorElement
-              type="bottom"
-              isLocked={true}
-              text={data[0].name + ' (низ)'}
-              price={data[0].price}
-              thumbnail={data[0].image}
-            />
+        <div className={styles.wrapMiddleInner}>
+          <div className={styles.wrap + ' ' +styles.wrapTop}>
+              <ConstructorElement
+                type="top"
+                isLocked={true}
+                text={data[0].name + ' (верх)'}
+                price={data[0].price}
+                thumbnail={data[0].image}
+              />
+          </div>
+          <div className={styles.wrapMiddle+ " mt-4 mb-4"} >
+            {
+              data.filter(item => item.type !== 'bun').map(cnstructorElement => {
+                return (
+                  <div key={cnstructorElement._id}>
+                    <DragIcon type="primary" />
+                    <ConstructorElement
+                      text={cnstructorElement.name}
+                      price={cnstructorElement.price}
+                      thumbnail={cnstructorElement.image}
+                    />
+                  </div>
+                )
+              })
+            }
+          </div>
+          <div className={styles.wrap + ' ' +styles.wrapBottom}>
+              <ConstructorElement
+                type="bottom"
+                isLocked={true}
+                text={data[0].name + ' (низ)'}
+                price={data[0].price}
+                thumbnail={data[0].image}
+              />
+          </div>
         </div>
         <div className={styles.priceBlock + ' mt-10'}>
           <div className="text text_type_digits-medium">
